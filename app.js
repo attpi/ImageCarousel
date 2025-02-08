2c9088cfbec16f6bf59f9248bd06a37feb377904
Chun-Hsiang Yu <kihifung@gmail.com>
2025-02-08 15:27:34 +0800

    Enable slide movement for images (right)

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // 程式碼寫這裡
+const slides = document.querySelectorAll(".slide");
+const track = document.querySelector(".slide-track");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+let currentIndex = 0;
+
+function setupSlides() {
+  const w = track.clientWidth; // 抓取投影片框的寬度
+
+  slides.forEach((slide, i) => {
+    // 對每張投影片往左移一張投影片的距離
+    slide.style.left = `${i * w}px`;
+  });
+}
+
+nextBtn.addEventListener("click", () => {
+  currentIndex++;
+
+  const w = track.clientWidth; // 抓取投影片框的寬度
+  track.style.transform = `translateX(-${currentIndex * w}px)`;
+});
+
+setupSlides();
